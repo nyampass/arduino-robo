6934fff2f819d00f82b92f0c772be0589014e1bb
Tokusei Noborio <tokusei@nyampass.com>
2018-10-28 16:15:47 +0900

    Remove \r in atp serial

--- halake_buggy_sound_project/halake_buggy_sound_project.ino
+++ halake_buggy_sound_project/halake_buggy_sound_project.ino
@@ -35,7 +35,7 @@ void setup() {
   delay(500);
 
   Serial.println("設定完了");
-  softSerial.println("setteikannryou\r");
+  softSerial.println("setteikannryou");
 }
 
 void loop() {
@@ -48,3 +48,4 @@ void loop() {
     }
   }
 }
+g
